3d377415ac74409ce6e5fad9faed1b64ab91b80f
MartinXCVI <mdzg96@gmail.com>
2024-11-28 00:49:27 -0300

    Added routes, main views, main layout, and partial components

--- server/routes/main.js
+++ server/routes/main.js
@@ -4,7 +4,27 @@ const router = express.Router()
 
 // Routes
 router.get('/', (req, res)=> {
-  res.send('Hello World')
+  const locals = {
+    title: "Node Blog",
+    description: "Simple blog created with Node/Express/Mongo"
+  }
+  res.render('index', { locals })
+})
+
+router.get('/about', (req, res)=> {
+  const locals = {
+    title: "Node Blog: About",
+    description: "Simple blog created with Node/Express/Mongo"
+  }
+  res.render('about', { locals })
+})
+
+router.get('/contact', (req, res)=> {
+  const locals = {
+    title: "Node Blog: Contact",
+    description: "Simple blog created with Node/Express/Mongo"
+  }
+  res.render('contact', { locals })
 })
 
 module.exports = router
